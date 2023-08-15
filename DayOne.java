509b54e2afa2d168bf280f5ae311945873654f8a
Ansh Singh Sonkhia <anshssonkhia@gmail.com>
2023-08-15 17:17:28 +0530

    Learning Java Day 1

---
+++ DayOne.java
@@ -0,0 +1,33 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java habibi");  
+     System.out.println("Power | Tsunami");
+     System.out.println(2+65);
+
+     // Variables
+     // DataType VariableName = value;
+
+     int val1 = 11;
+     float val2 = val1;
+     float val3 = 331.1f;
+     int val4 = 'a';
+     int val5 = 'A';
+     char val6 = 'a';
+
+     float sum = val2 + val3;
+
+     System.out.println(val4);
+     System.out.println(val5);
+     System.out.println(val6);
+
+     System.out.println("Sum is: " + sum);
+     
+     String myString = "Ansh Singh Sonkhia";
+
+     System.out.println("Summ... = " + myString);
+
+
+     // String is a class --> (first letter Capital)
+     // int, char, void, float --> are data type --> (first letter small)
+    }  
+}  
